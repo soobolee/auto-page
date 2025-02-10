3bab3ab13c5cf38b424d8ef720a4fc6735ebedd4
SooBo <sjstnqh@naver.com>
2025-02-10 11:32:47 +0900

    design: Navigation 컴포넌트 생성

---
+++ src/renderer/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+function Navigation() {
+  return (
+    <nav className="w-[20%] h-[90%] border p-14">
+      <ul className="flex justify-center items-center flex-col">
+        <li>Home</li>
+        <li>Bookmark</li>
+        <li>Add Macro</li>
+        <li>Short Cut</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
