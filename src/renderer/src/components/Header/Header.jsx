c42ffc0bddcd0fe3e43d4c80b4eb213e3dee0a86
SooBo <sjstnqh@naver.com>
2025-02-10 14:52:55 +0900

    feat: electron app drag region 추가

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 function Header() {
   return (
-    <header className="h-[10%] flex justify-center items-center border">
+    <header
+      className="h-[10%] flex justify-center items-center border"
+      style={{"-webkit-app-region": "drag"}}
+    >
       <h3 className="text-white text-2xl">Auto Pape</h3>
     </header>
   );
