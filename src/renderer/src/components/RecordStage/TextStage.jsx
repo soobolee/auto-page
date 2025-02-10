00c7a1386adb3594a1d0f90200e7b76a45a52579
SooBo <sjstnqh@naver.com>
2025-02-10 20:31:59 +0900

    design: Stage영역 컴포넌트 생성

---
+++ src/renderer/src/components/RecordStage/TextStage.jsx
@@ -0,0 +1,9 @@
+function TextStage() {
+  return (
+    <aside className="w-full h-full p-5 flex flex-row border col-span-8">
+      <div id="textStage" className="w-[17%] h-full mx-3 border"></div>
+    </aside>
+  );
+}
+
+export default TextStage;
