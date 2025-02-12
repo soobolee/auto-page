706defa45fcf8d5d6956581551a19adf4e168634
SooBo <sjstnqh@naver.com>
2025-02-12 15:04:03 +0900

    fix: a태그 새탭 _blank타겟에만 적용

--- src/preload/index.js
+++ src/preload/index.js
@@ -20,7 +20,7 @@ try {
       const buttonTag = event.target.closest("button");
       const iButtonTag = event.target.closest("input[type='button']");
 
-      if (aTag) {
+      if (aTag && aTag.target === "_blank") {
         ipcRenderer.sendToHost("new-tab", aTag.href);
       }
       if (buttonTag) {
