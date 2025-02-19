7c607437926f9170856105455cb7c0822d9b8bef
SooBo <sjstnqh@naver.com>
2025-02-19 23:57:21 +0900

    refactor: toggle함수 가독성에 좋지 않아 삭제

--- src/renderer/src/stores/useMacroStageStore.js
+++ src/renderer/src/stores/useMacroStageStore.js
@@ -8,14 +8,8 @@ const useMacroStageStore = create((set) => ({
   setMacroStageList: (newStageList) => set({macroStageList: newStageList}),
   setImageStageList: (newImageList) => set({macroImageList: newImageList}),
   resetStageList: () => set({macroStageList: [], macroImageList: [], isMacroExecuting: false}),
-  toggleMacroExecute: () =>
-    set((state) => ({
-      isMacroExecuting: !state.isMacroExecuting,
-      macroStageList: state.isMacroExecuting ? state.macroStageList : [],
-    })),
   startMacroExecute: () => set({isMacroExecuting: true}),
   stopMacroExecute: () => set({isMacroExecuting: false, macroStageList: []}),
-  toggleMacroRecord: () => set((state) => ({isMacroRecording: !state.isMacroRecording})),
   startMacroRecord: () => set({isMacroRecording: true}),
   stopMacroRecord: () => set({isMacroRecording: false}),
 }));
--- src/renderer/src/stores/useUserConfigStore.js
+++ src/renderer/src/stores/useUserConfigStore.js
@@ -3,7 +3,6 @@ import {create} from "zustand";
 const useUserConfigStore = create((set) => ({
   isShowModal: false,
   shortCutUnitList: [],
-  toggleModal: () => set((state) => ({isShowModal: !state.isShowModal})),
   openModal: () => set({isShowModal: true}),
   closeModal: () => set({isShowModal: false}),
   setShortCutUnitList: (newShortCutUnit) => set({shortCutUnitList: newShortCutUnit}),
