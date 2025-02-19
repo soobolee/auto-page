fcac49b70c492e4ac43e4207158764def14e3c3d
SooBo <sjstnqh@naver.com>
2025-02-19 22:34:56 +0900

    feat: url 검증 기능 추가

--- src/renderer/src/components/Content/InputUrlContent.jsx
+++ src/renderer/src/components/Content/InputUrlContent.jsx
@@ -1,9 +1,44 @@
+import {useState} from "react";
 import useTabStore from "../../stores/useTabStore";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSquarePlus} from "@fortawesome/free-solid-svg-icons";
 
 function InputUrlContent() {
+  const [inputUrl, setInputUrl] = useState("");
+  const [isUrlError, setIsUrlError] = useState("");
   const {setBrowserTabList} = useTabStore();
+
+  const validationHttpsUrl = (urlString) => {
+    try {
+      const url = new URL(urlString);
+
+      if (url.protocol !== "https:" || url.protocol !== "http:") {
+        return false;
+      }
+
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const handleInputEnter = (event) => {
+    if (event.key === "Enter") {
+      const isUrl = validationHttpsUrl(inputUrl);
+
+      if (isUrl) {
+        setIsUrlError(false);
+        setBrowserTabList([{tabUrl: inputUrl}]);
+      } else {
+        setInputUrl("");
+        setIsUrlError(true);
+      }
+    }
+  };
+
+  const handleInput = (event) => {
+    setInputUrl(event.target.value);
+  };
 
   return (
     <article className="flex items-center flex-col col-span-7">
@@ -12,12 +47,10 @@ function InputUrlContent() {
         <input
           type="text"
           className="w-[50%] h-14 p-4 rounded-4xl bg-white placeholder:italic overflow-auto"
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              setBrowserTabList([{tabUrl: "https://naver.com"}]);
-            }
-          }}
-          placeholder="매크로 기록 URL 입력"
+          value={inputUrl}
+          onKeyDown={handleInputEnter}
+          onChange={handleInput}
+          placeholder={isUrlError ? "잘못된 URL을 입력했습니다." : "매크로 기록 URL 입력"}
         />
       </div>
       <div>
